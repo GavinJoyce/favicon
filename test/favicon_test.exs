d0246e04a7126660853c119667d81465c3d1e48f
Richard Nyström <ricn@rny.io>
2015-10-27 21:04:55 +0100

    clean up tests

--- test/favicon_test.exs
+++ test/favicon_test.exs
@@ -14,40 +14,40 @@ defmodule FaviconTest do
     assert result == "#{url}/icon.svg"
   end
 
-  test "try fetching favicon from site without favicon" do
-    url = "http://lasmastarn.se"
-    assert {:error, :favicon_missing} == Favicon.fetch(url)
-  end
-
-  test "fetch one without link tag in body" do
+  test "fetch one with missing link tag" do
     url = "http://hexdocs.pm/ecto/Ecto.html"
     {:ok, result} = Favicon.fetch(url)
     assert result == "http://hexdocs.pm/favicon.ico"
   end
 
-  test "try with non existent domain" do
+  test "try fetch with non existent domain" do
     url = "http://thisurldoesnotexists.com"
     {:error, :nxdomain} = Favicon.fetch(url)
   end
 
-  test "non authorized url" do
+  test "try fetch favicon from site without favicon" do
+    url = "http://lasmastarn.se"
+    assert {:error, :favicon_missing} == Favicon.fetch(url)
+  end
+
+  test "fetch non authorized url" do
     url = "https://trello.com/b/OeZ2BmXa/blackdocio"
     {:ok, result} = Favicon.fetch(url)
     assert result == "https://trello.com/favicon.ico"
   end
 
-  test "404 url" do
+  test "fetch 404 url" do
     url = "https://trello.com/molasricny"
     {:ok, result} = Favicon.fetch(url)
     assert result == "https://trello.com/favicon.ico"
   end
 
-  test "try with a non html url" do
+  test "fetch a non html url" do
     url = "http://a-eon.biz/PDF/News_Release_Developer.pdf"
     {:ok, result} = Favicon.fetch(url)
     assert result == "http://a-eon.biz/images/favicon.png"
   end
 
-  # try with differnt port
-  # try some sort of subdomain
+  # TODO: Add a test to see how it behaves with a different port (not 80)
+  # TODO: Add a test to see hwo it behaves with subdomains.
 end
