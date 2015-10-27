69a202908ef59ba7cc62c6172b824259981eaea5
Richard Nyström <ricn@rny.io>
2015-10-27 21:05:05 +0100

    Mix fix

--- mix.exs
+++ mix.exs
@@ -3,31 +3,35 @@ defmodule Favicon.Mixfile do
 
   def project do
     [app: :favicon,
-     version: "0.9.0",
+     version: "0.0.1",
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: description,
+     package: package,
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [{:httpoison, "~> 0.7.2"},
      {:floki, "~> 0.6"}]
   end
+
+  defp description do
+   """
+   A library that fetches the favicon url for a specified url
+   """
+ end
+
+ defp package do
+   [
+    files: ["lib", "priv", "mix.exs", "README*", "readme*", "LICENSE*", "license*"],
+    maintainers: ["Richard Nyström"],
+    licenses: ["MIT"],
+    links: %{"GitHub" => "https://github.com/ricn/favicon"}]
+ end
 end
