0c881231bc3298194f150539ac286e0f0f3c47d4
Richard Nyström <ricn@rny.io>
2015-10-27 23:55:48 +0100

    Version bump

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Favicon.Mixfile do
 
   def project do
     [app: :favicon,
-     version: "0.0.2",
+     version: "0.0.3",
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
